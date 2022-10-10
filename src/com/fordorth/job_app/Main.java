58a1dcb54cbd04a0ef338e70f31a4d75d022c318
firebird2552 <firebird2552@gmail.com>
2022-10-10 14:56:09 -0700

    Added documentation and Warning

--- src/com/fordorth/job_app/Main.java
+++ src/com/fordorth/job_app/Main.java
@@ -1,5 +1,24 @@
 package com.fordorth.job_app;
 
+/**
+ * USE AT YOUR OWN RISK!!!!
+ * This application is for EDUCATIONAL purposes only
+ * This application was written for public use as practice.
+ * This is not a production application.
+ * This application is not intended to be used for the purpose it was written.
+ * Any negative account activities that happen because of this app are the user's responsibility.
+ *
+ * Author:  Fordorth A.K.A. Firebird2552
+ * Date:    10/10/2022
+ * License: All Rights Reserved
+ * Purpose: Scrub through job application websites like LinkedIn, Indeed, etc. determine if the user qualifies for the
+ *      job and applies if the job meets all interests like Salary, Location, etc.
+ *      Goals:
+ *          Version 1: a terminal interface that takes a file that is a resume file, salary range, and location preference
+ *          creates a file of jobs applied to
+ *          creates a file of jobs marked no interested
+ *          Version 2: A GUI and a database to store applied jobs
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
